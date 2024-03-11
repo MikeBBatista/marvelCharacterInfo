7c562ad94811719a6aeeb3203777c886e8a62683
Mike Barcelos Batista <mike.batista@codex.com.br>
2024-03-11 14:07:03 -0300

    adicionando rotas ao projeto e criando a tela de informacoes do personagem

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { CharacterInfoComponent } from './pages/character-info/character-info.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
+import { CharacterListComponent } from './pages/character-list/character-list.component';
+
+const routes: Routes = [
+  //Since it's a small application, I didn't apply the modularity with which I could apply lazy loading.
+  {
+    path: '',
+    component: CharacterListComponent
+  },
+  {
+    path: 'characters/:id',
+    component: CharacterInfoComponent
+  },
+  {
+    path: '**',
+    component: PageNotFoundComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/pages/character-info/character-info.component.ts
+++ src/app/pages/character-info/character-info.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { throwError } from 'rxjs';
+
+import { MarvelCharacterService } from 'src/app/core/service/marvel-character.service';
+import { Character } from 'src/app/core/models/characters-models';
 
 @Component({
   selector: 'app-character-info',
@@ -7,9 +12,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CharacterInfoComponent implements OnInit {
 
-  constructor() { }
+  public character!: Character;
+  public onLoad: boolean = true;
+  public imgSrc: string = '';
+  public moreInfo: string = '';
+
+  constructor(
+    private route: ActivatedRoute, 
+    private marvelCharService: MarvelCharacterService,
+    ) 
+  { }
 
   ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const characterName = params['id'];
+      this.marvelCharService.getCharacterById(characterName).subscribe({
+        next: data => {
+          this.character = data?.data?.results[0];
+          this.onLoad = false;
+          this.imgSrc = `${this.character.thumbnail.path}.${this.character.thumbnail.extension}`
+          this.character.urls.forEach(item => {
+            if (item.type === "comiclink") {
+                this.moreInfo = item.url;
+            }
+          });
+        },
+        error: () => {
+          const errorMessage = 'Ocorreu um erro ao carregar os personagens. Por favor, tente novamente mais tarde.';
+          this.onLoad = false;
+          return throwError(() => new Error(errorMessage))
+        }
+      })
+    })
   }
-
 }
