5cf0428a5a2dbae8fd11767815150c6a8e91f39d
Mike Barcelos Batista <mike.batista@codex.com.br>
2024-03-11 17:51:39 -0300

    subindo arquivo de environmento corretamente

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
   apiUrl: 'https://gateway.marvel.com',
-  marvelPublicKey: 'b3c49dfed8392b27cc65542d064286fd',
+  marvelPublicKey: '',
   marvelPrivateKey: '',
 };
