f48bb06fad3279e29884158f72ce82c445035c2f
Mike Barcelos Batista <mike.batista@codex.com.br>
2024-03-11 11:30:29 -0300

    criando servico de cache para aplicacao

--- src/app/core/service/cache.service.ts
+++ src/app/core/service/cache.service.ts
@@ -1,9 +1,54 @@
 import { Injectable } from '@angular/core';
+import { CacheCharactersList } from '../models/cache-models';
+import { CacheKeys } from '../enums/cache-keys.enum'
+import { Character } from '../models/characters-models';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CacheService {
 
-  constructor() { }
+  private cache: { [key: string]: any } = {};
+
+  set(key: string, value: any) {
+    this.cache[key] = value;
+  }
+
+  get(key: string): any {
+    return this.cache[key];
+  }
+
+  clear() {
+    this.cache = {};
+  }
+
+  setCachedCharacterList(data: Character[], page: number, totalItems: number) {
+    this.set(CacheKeys.Characters, data);
+    this.set(CacheKeys.PageNumber, page);
+    this.set(CacheKeys.TotalItems, totalItems);
+  }
+
+  setCachedSelectOptions(data: Character[], page: number, totalItems: number) {
+    this.set(CacheKeys.selectFilterOptions, data);
+    this.set(CacheKeys.selectFilterPage, page);
+    this.set(CacheKeys.selectFilterTotalItems, totalItems);
+  }
+
+  getCachedSelectOptions() {
+    const cachedOptionsData: CacheCharactersList = {
+      characters: this.get(CacheKeys.selectFilterOptions),
+      pageNumber: this.get(CacheKeys.selectFilterPage),
+      totalItems: this.get(CacheKeys.selectFilterTotalItems)
+    }
+    return cachedOptionsData;
+  }
+
+  getCachedCharacterList() {
+    const cachedData: CacheCharactersList = {
+      characters: this.get(CacheKeys.Characters),
+      pageNumber: this.get(CacheKeys.PageNumber),
+      totalItems: this.get(CacheKeys.TotalItems)
+    };
+    return cachedData;
+  }
 }
