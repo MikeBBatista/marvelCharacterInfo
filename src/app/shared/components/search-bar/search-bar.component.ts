0cbf902563827f5a4a3104f56a363925c80b2de2
Mike Barcelos Batista <mike.batista@codex.com.br>
2024-03-12 09:26:56 -0300

    colocando debouceTime no searchBar

--- src/app/shared/components/search-bar/search-bar.component.ts
+++ src/app/shared/components/search-bar/search-bar.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { Subject, debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-search-bar',
@@ -9,6 +10,19 @@ export class SearchBarComponent {
 
   @Output() search: EventEmitter<String> = new EventEmitter();
   public searchInput: string = '';
+  private searchSubject: Subject<string> = new Subject<string>();
+
+  constructor() {
+    this.searchSubject.pipe(
+      debounceTime(500)
+    ).subscribe(() => {
+      this.doSearch();
+    });
+  }
+
+  onSearchInput(event: any) {
+    this.searchSubject.next(event.target.value);
+  }
 
 
   doSearch() {
